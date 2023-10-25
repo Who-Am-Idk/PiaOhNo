73c3276bc67876afbb4f3e8a37be2b4d666082ec
MarsMatthew <Matthewsorensen007@gmail.com>
2023-10-24 21:47:03 -0600

    WinForms0.01

--- src/WinForms/PiaOhForm/Form1.Designer.cs
+++ src/WinForms/PiaOhForm/Form1.Designer.cs
@@ -29,27 +29,102 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PiaOhNo));
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.whiteKeys = new System.Windows.Forms.TableLayoutPanel();
+            this.bButton = new System.Windows.Forms.Button();
+            this.aButton = new System.Windows.Forms.Button();
+            this.gButton = new System.Windows.Forms.Button();
+            this.fButton = new System.Windows.Forms.Button();
+            this.eButton = new System.Windows.Forms.Button();
+            this.dButton = new System.Windows.Forms.Button();
+            this.cButton = new System.Windows.Forms.Button();
+            this.whiteKeys.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tableLayoutPanel1
+            // whiteKeys
             // 
-            resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            resources.ApplyResources(this.whiteKeys, "whiteKeys");
+            this.whiteKeys.BackColor = System.Drawing.Color.DimGray;
+            this.whiteKeys.Controls.Add(this.bButton, 6, 0);
+            this.whiteKeys.Controls.Add(this.aButton, 5, 0);
+            this.whiteKeys.Controls.Add(this.gButton, 4, 0);
+            this.whiteKeys.Controls.Add(this.fButton, 3, 0);
+            this.whiteKeys.Controls.Add(this.eButton, 2, 0);
+            this.whiteKeys.Controls.Add(this.dButton, 1, 0);
+            this.whiteKeys.Controls.Add(this.cButton, 0, 0);
+            this.whiteKeys.Name = "whiteKeys";
+            this.whiteKeys.Paint += new System.Windows.Forms.PaintEventHandler(this.whiteKeysTable);
+            // 
+            // bButton
+            // 
+            this.bButton.BackColor = System.Drawing.Color.LavenderBlush;
+            resources.ApplyResources(this.bButton, "bButton");
+            this.bButton.Name = "bButton";
+            this.bButton.UseVisualStyleBackColor = false;
+            // 
+            // aButton
+            // 
+            this.aButton.BackColor = System.Drawing.Color.LavenderBlush;
+            resources.ApplyResources(this.aButton, "aButton");
+            this.aButton.Name = "aButton";
+            this.aButton.UseVisualStyleBackColor = false;
+            // 
+            // gButton
+            // 
+            this.gButton.BackColor = System.Drawing.Color.LavenderBlush;
+            resources.ApplyResources(this.gButton, "gButton");
+            this.gButton.Name = "gButton";
+            this.gButton.UseVisualStyleBackColor = false;
+            // 
+            // fButton
+            // 
+            this.fButton.BackColor = System.Drawing.Color.LavenderBlush;
+            resources.ApplyResources(this.fButton, "fButton");
+            this.fButton.Name = "fButton";
+            this.fButton.UseVisualStyleBackColor = false;
+            // 
+            // eButton
+            // 
+            this.eButton.BackColor = System.Drawing.Color.LavenderBlush;
+            resources.ApplyResources(this.eButton, "eButton");
+            this.eButton.Name = "eButton";
+            this.eButton.UseVisualStyleBackColor = false;
+            // 
+            // dButton
+            // 
+            this.dButton.BackColor = System.Drawing.Color.LavenderBlush;
+            resources.ApplyResources(this.dButton, "dButton");
+            this.dButton.Name = "dButton";
+            this.dButton.UseVisualStyleBackColor = false;
+            // 
+            // cButton
+            // 
+            this.cButton.BackColor = System.Drawing.Color.LavenderBlush;
+            resources.ApplyResources(this.cButton, "cButton");
+            this.cButton.Name = "cButton";
+            this.cButton.UseVisualStyleBackColor = false;
             // 
             // PiaOhNo
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.whiteKeys);
             this.Name = "PiaOhNo";
+            this.whiteKeys.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel whiteKeys;
+        private System.Windows.Forms.Button cButton;
+        private System.Windows.Forms.Button bButton;
+        private System.Windows.Forms.Button aButton;
+        private System.Windows.Forms.Button gButton;
+        private System.Windows.Forms.Button fButton;
+        private System.Windows.Forms.Button eButton;
+        private System.Windows.Forms.Button dButton;
     }
 }
 
--- src/WinForms/PiaOhForm/Form1.cs
+++ src/WinForms/PiaOhForm/Form1.cs
@@ -10,11 +10,33 @@ using System.Windows.Forms;
 
 namespace PiaOhForm
 {
+    /*
+     * Pia-Oh-No!
+     * Written By: Matthew Sorensen
+     * 10/24/2023
+     * GTI Programming 1-2 Class project
+     * Winforms Edition
+     */
     public partial class PiaOhNo : Form
     {
+        
         public PiaOhNo()
         {
             InitializeComponent();
         }
+
+        private void whiteKeysTable(object sender, PaintEventArgs e)
+        {
+            //Figuring this out took way too long.
+            int displayH = ClientRectangle.Height;
+            int displayW = ClientRectangle.Width;
+            //Want the margin to be 3% of the width of the window.
+            //Also want the top of the keyboard to be about down about 60.5% of the way. 
+            double[] pianoAnchor = {0.03*displayW, .605*displayH}; // Can't cast to integer inside of the array. It breaks.
+            whiteKeys.Location = new Point((int)pianoAnchor[0], (int)pianoAnchor[1]);
+            whiteKeys.Size = new Size(displayW - (int)pianoAnchor[0]*2, displayH); 
+            whiteKeys.BackColor = Color.Pink;
+        }
+        //Figure out arrays and button click.. thing... im tired, going to bed.
     }
 }
